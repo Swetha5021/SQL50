e758d8265f8418b288702a49b636f3d1b4ca9275
Swetha S <swethasivasankararaj.05@gmail.com>
2024-06-20 18:45:45 +0530

    added programs

---
+++ ProductSalesAnalysis-I.sql
@@ -0,0 +1,3 @@
+select Product.product_name, Sales.year, Sales.price 
+from Sales 
+left join Product on Product.product_id=Sales.product_id;
---
+++ ReplaceEmployeeID.sql
@@ -0,0 +1,3 @@
+select EmployeeUNI.unique_id, Employees.name 
+from Employees 
+left join EmployeeUNI on Employees.id=EmployeeUNI.id;
